6611deb7b944e967c2dc664b84f40083e858e1d5
Runali Behera <runalibehera17@gmail.com>
2026-07-24 00:01:06 +0530

    Time: 0 ms (100.00%) | Memory: 24.8 MB (88.17%) - LeetSync

--- 3324-split-the-array/split-the-array.cpp
+++ 3324-split-the-array/split-the-array.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool isPossibleToSplit(vector<int>& nums) {
-        // Fixed-size array to map frequencies of numbers 1 to 100
+       
         vector<int> track(100, 0);
 
         for(int num : nums) {
             track[num - 1]++;
-            // If any value appears 3 or more times, it cannot be split into 2 unique arrays
+            
             if(track[num - 1] > 2) {
                 return false;
             }
