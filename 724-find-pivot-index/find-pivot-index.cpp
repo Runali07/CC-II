bed78bcd1105c950b879c74cb6333b09331c5d5a
Runali Behera <runalibehera17@gmail.com>
2026-08-11 19:05:32 +0530

    Time: 758 ms (5.05%) | Memory: 35.8 MB (30.21%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            int left=0;
-            int right =0;
-            for(int j=0;j<i;j++){
-                left+=nums[j];
-            }
-            for(int j=i+1;j<n;j++){
-                right+=nums[j];
-            }
-            if(left==right){
-                return i;
-            }
+       int n=nums.size();
+       for(int i=0;i<n;i++){
+        int low=0;
+        int high=0;
+        for(int j=0;j<i;j++){
+            low+=nums[j];
         }
-        return -1;
+        for(int j=i+1;j<n;j++){
+            high+=nums[j];
+        }
+        if(low==high){
+            return i;
+        }
+       }
+       return -1;
     }
 };
